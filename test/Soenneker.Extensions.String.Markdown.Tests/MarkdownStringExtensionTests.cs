77a7db0032753d2264946b9a36b7028be239357b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:05 -0500

    TUnit conversion

--- test/Soenneker.Extensions.String.Markdown.Tests/MarkdownStringExtensionTests.cs
+++ test/Soenneker.Extensions.String.Markdown.Tests/MarkdownStringExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.String.Markdown.Tests;
 
 public class MarkdownStringExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
